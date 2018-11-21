9fe5d175b92737bd78da5872962fb2c9e287bc67
Enrique Crespo <jecrespom@gmail.com>
2018-11-21 18:46:26 +0100

    fix countdown

--- publish_cuenta_atras/publish_cuenta_atras.ino
+++ publish_cuenta_atras/publish_cuenta_atras.ino
@@ -1,12 +1,13 @@
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
 
 // Direccion MAC
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xYY };  //Sustituir YY por el número de kit
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x21 };  //Sustituir YY por el número de kit
 
 // IP del servidor
-IPAddress mqtt_server(192, 168, X, X); //poner la IP de mosquitto al que conectarse
+IPAddress mqtt_server(192, 168, 6, 72); //poner la IP de mosquitto al que conectarse
 
 // Topic con el que trabajamos
 const char* topicName = "countdown";
@@ -27,18 +28,21 @@ void loop()
 {
   if (!client.connected()) {
     Serial.print("Connecting ...\n");
-    client.connect("arduinoClientXX", "curso_iot", "raspberry");  //Sustituir XX por número de puesto
-  }
-  else {
+    client.connect("arduinoClient21", "curso_iot", "raspberry");  //Sustituir XX por número de puesto
     // Envio la cuenta atras
-    for (int i = 10; i <= 0; i--) {
+    for (int i = 10; i >= 0; i--) {
       char buffer[5];
       sprintf(buffer, "%i", i);
       Serial.print("Envio la cadena de caracteres: ");
       Serial.println(buffer);
       client.publish(topicName, buffer);
+      delay(1000);
     }
   }
+  else {
+    Serial.println("Fallo conexión");
+  }
+
   // Tiempo entre envios (en ms)
   delay(10000);
 }
